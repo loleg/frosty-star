a9f4af2264da1714df00ac788a22fc92566165ce
Oleg Lavrovsky <oleg@datalets.ch>
2021-09-07 16:05:23 +0200

    vite happy

--- src/views/Scan.vue
+++ src/views/Scan.vue
@@ -14,9 +14,9 @@ import Camera from "@/components/Camera.vue";
           <router-link
             to="/scan"
             class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none"
-            >Start Again</router-link
-          >
+            >Start Again</router-link>
         </div>
+      </div>
     </div>
   </div>
 </template>
